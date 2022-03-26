45d46ced70fc18cc5b87bdd1f6c4e4f31fbcb323
palloncino <antonio.guiotto.dev@gmail.com>
2022-03-26 18:11:37 +0100

    fixes

--- src/features/components/Entities/index.tsx
+++ src/features/components/Entities/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link, Outlet } from 'react-router-dom';
+import _ from 'lodash';
 
 export const Entities = () => {
   return (
     <div style={{ background: 'rgba(255, 0, 0, 0.1)' }}>
       <h3>List of ids</h3>
       <ul>
-        {['id-101', 'id-102'].map(id => {
+        {['id-101', 'id-102'].map((id) => {
           return (
-            <li>
+            <li key={_.uniqueId()}>
               <Link to={id}>{id}</Link>
             </li>
           )
